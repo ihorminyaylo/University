84120cccbe71f3dcb019c533f482f34b989074aa
ihorminyaylo <ihor.minyaylo@gmail.com>
2017-07-31 13:21:42 +0300

    First work with my batis

---
+++ src/test/java/junit/StudentServiceDBTest.java
@@ -0,0 +1,14 @@
+package junit;
+
+import servicesDB.StudentServiceDB;
+
+/**
+ * Created by AsusIT on 30.07.2017.
+ */
+public class StudentServiceDBTest {
+    private static StudentServiceDB studentServiceDB;
+
+    @BeforeClas
+
+
+}
